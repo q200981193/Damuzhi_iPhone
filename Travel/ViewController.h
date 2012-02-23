e818a3dbfbbc91934bfe2d1136d3664f09032624
Benson Peng <penglingzhe@gmail.com>
2012-02-23 21:35:00 +0800

    init

---
+++ Travel/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Travel
+//
+//  Created by  on 12-2-23.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
