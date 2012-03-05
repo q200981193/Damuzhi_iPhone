ded74327b417ffe0f59318e38e5aa0100eb2f1bc
Benson Peng <penglingzhe@gmail.com>
2012-03-05 12:24:48 +0800

    add files and update protocol files

---
+++ Travel/Controller/Common/PlaceMapViewController.h
@@ -0,0 +1,15 @@
+//
+//  PlaceMapViewController.h
+//  Travel
+//
+//  Created by  on 12-3-5.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlaceMapViewController : UIViewController
+
+- (void)setPlaces:(NSArray*)placeList;
+
+@end
