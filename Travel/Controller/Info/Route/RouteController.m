ae41ec864d481ff62169d89b57be27ffd74fb505
xiaotaowang <445242970@qq.com>
2012-04-10 16:46:46 +0800

    add routeController

---
+++ Travel/Controller/Info/Route/RouteController.m
@@ -0,0 +1,67 @@
+//
+//  RouteController.m
+//  Travel
+//
+//  Created by 小涛 王 on 12-4-10.
+//  Copyright (c) 2012年 甘橙软件. All rights reserved.
+//
+
+#import "RouteController.h"
+
+@implementation RouteController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    
+//    self.dataList = []
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+#pragma mark - 
+#pragma mark: Table View Delegate
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+	return 54;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;		// default implementation
+}
+
+// Customize the number of rows in the table view.
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [dataList count];
+}
+
+// Customize the appearance of table view cells.
+- (UITableViewCell *)tableView:(UITableView *)theTableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    int row = [indexPath row];	
+	int count = [dataList count];
+	if (row >= count){
+		NSLog(@"[WARN] cellForRowAtIndexPath, row(%d) > data list total number(%d)", row, count);
+		return nil;
+	}
+    
+    UITableViewCell *cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"CellForRoute"] autorelease];
+    
+    
+    
+  	return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+
+}
+
+
+
+@end
---
+++ Travel/Model/TravelTips/TravelTips.m
@@ -0,0 +1,13 @@
+//
+//  TravelTips.m
+//  Travel
+//
+//  Created by 小涛 王 on 12-4-10.
+//  Copyright (c) 2012年 甘橙软件. All rights reserved.
+//
+
+#import "TravelTips.h"
+
+@implementation TravelTips
+
+@end
