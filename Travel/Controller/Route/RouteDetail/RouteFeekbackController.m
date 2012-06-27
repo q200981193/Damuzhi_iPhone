f07900170debc4accfd4b2a9b24d46d7eb35ebdb
kira <ouyongyong@163.com>
2012-06-27 12:01:25 +0800

    commit

--- Travel/Controller/Route/RouteDetail/RouteFeekbackController.m
+++ Travel/Controller/Route/RouteDetail/RouteFeekbackController.m
@@ -8,19 +8,37 @@
 
 #import "RouteFeekbackController.h"
 #import "Package.pb.h"
+#import "PPNetworkRequest.h"
+#import "RouteFeekbackCell.h"
+#import "ImageManager.h"
+
+#import "AppManager.h"
+
+#define TAG_DEPART_CITY_LABEL 18
 
 #define EACH_COUNT 20
 
 @interface RouteFeekbackController ()
 
 @property (assign, nonatomic) int routeId;
+@property (assign, nonatomic) int start;
+@property (retain, nonatomic) NSMutableArray *allRouteFeekback;
+@property (assign, nonatomic) int totalCount;
+@property (assign, nonatomic) int departCityId;
+@property (assign, nonatomic) int statisticsView;
 
+
+- (void)updateStatisticsData;
 @end
 
 @implementation RouteFeekbackController
 
 @synthesize routeId = _routeId;
-
+@synthesize start = _start;
+@synthesize allRouteFeekback = _allRouteFeekback;
+@synthesize totalCount = _totalCount;
+@synthesize departCityId = _departCityId;
+@synthesize statisticsView = _statisticsView;
 - (id)initWithRouteId:(int)routeId
 {
     if (self = [super init]) {
@@ -40,13 +58,10 @@
                         imageName:@"back.png" 
                            action:@selector(clickBack:)];
     
-    [self setNavigationRightButton:NSLS(@"我的关注") 
+    [self setNavigationRightButton:NSLS(@"咨询") 
                          imageName:@"topmenu_btn2.png"
-                            action:@selector(clickMyFollow:)];    
-    
-    
-    
-    
+                            action:@selector(query:)];
+    self.navigationItem.title = NSLS(@"路线详情");
     
     
     [RouteService defaultService] ;
@@ -62,23 +77,106 @@
     // e.g. self.myOutlet = nil;
 }
 
+
+
+- (UITableViewCell*) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = nil;
+    cell = [tableView dequeueReusableCellWithIdentifier:[RouteFeekbackCell getCellIdentifier]];
+    if (cell == nil) {
+        cell = [RouteFeekbackCell createCell:self];		
+        
+        UIImageView *view = [[UIImageView alloc] init];
+        [view setImage:[[ImageManager defaultManager] listBgImage]];
+        [cell setBackgroundView:view];
+        [view release];
+        // Customize the appearance of table view cells at first time
+    }
+    
+    int row = [indexPath row];	
+    int count = [dataList count];
+    if (row >= count){
+        PPDebug(@"[WARN] cellForRowAtIndexPath, row(%d) > data list total number(%d)", row, count);
+        return cell;
+    }
+    
+    RouteFeekbackCell* routeCell = (RouteFeekbackCell*)cell;
+    [routeCell setCellData:[dataList objectAtIndex:row]];
+    return cell;
+}
+
+
+
+// TableView delegate
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    //return [RouteFeekbackController getCellHeight];
-
-
-//- (void)findRequestDone:(int)result totalCount:(int)totalCount routeList:(NSArray *)routeList
-//{
-//    self.dataList = routeList;
-//}
+    return [RouteFeekbackCell getCellHeight];
 
 }
 
 
-- (void)findRequestDone:(int)result totalCount:(int)totalCount routeList:(NSArray *)routeList
+
+- (void)updateStatisticsData
 {
-    self.dataList = routeList;
+//    NSString *departCityName = [[AppManager defaultManager] getDepartCityName:_departCityId];
+//    UILabel *departCityLabel = (UILabel *)[_statisticsView viewWithTag:TAG_DEPART_CITY_LABEL];
+//    [departCityLabel setText:[NSString stringWithFormat:@"%@出发", departCityName]];
+    
+    //    int agencyCount = 0;
+    //    if ([[_selectedItemIds.agencyIds objectAtIndex:0] intValue] == ALL_CATEGORY) {
+    //        agencyCount = [[[AppManager defaultManager] getAgencyItemList:dataList] count] - 1;
+    //    }else {
+    //        [_selectedItemIds.agencyIds count];
+    //    }
+    //    UILabel *agencyLabel = (UILabel *)[_statisticsView viewWithTag:TAG_AGENCY_LABEL];
+    //    [agencyLabel setText:[NSString stringWithFormat:@"%d个旅行社", agencyCount]];
+    //    
+    //    UILabel *routeCountLabel = (UILabel *)[_statisticsView viewWithTag:TAG_ROUTE_COUNT_LABEL];
+    //    [routeCountLabel setText:[NSString stringWithFormat:@"%d条线路", [dataList count]]];
 }
+
+// RouteService delegate
+- (void)findRequestDone:(int)result totalCount:(int)totalCount list:(NSArray *)routeFeekback
+{
+    [self dataSourceDidFinishLoadingNewData];
+    [self dataSourceDidFinishLoadingMoreData];
+    
+    if (result != ERROR_SUCCESS) {
+        [self popupMessage:@"网络弱，数据加载失败" title:nil];
+        return;
+    }
+    
+    if (_start == 0) {
+        self.noMoreData = NO;
+        [self.allRouteFeekback removeAllObjects];
+        self.dataList = [NSArray array];
+    }
+  
+    self.start += [routeFeekback count];
+    self.totalCount = totalCount;
+    
+    [_allRouteFeekback addObjectsFromArray:routeFeekback];
+    self.dataList = [dataList arrayByAddingObjectsFromArray:routeFeekback];     
+    
+    
+    if (_start >= totalCount) {
+        self.noMoreData = YES;
+    }
+    
+    [self updateStatisticsData];
+    
+    [dataTableView reloadData];
+}
+
 
 @end
+
+
+
+
+
+
+
+
+
 
