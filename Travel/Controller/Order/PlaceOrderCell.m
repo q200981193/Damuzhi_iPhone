87a78cea597431d19e69020dcda1589b4861812a
haodong <qiuhaodong2006@126.com>
2012-07-13 18:36:24 +0800

    update PlaceOrderController

--- Travel/Controller/Order/PlaceOrderCell.m
+++ Travel/Controller/Order/PlaceOrderCell.m
@@ -7,8 +7,63 @@
 //
 
 #import "PlaceOrderCell.h"
+@interface PlaceOrderCell()
+
+- (void)clickLeftButton:(id)sender;
+- (void)clickRightButton:(id)sender;
+
+@end
+
 
 @implementation PlaceOrderCell
+@synthesize backgroundImageView;
+@synthesize pointImageView;
+@synthesize titleLabel;
+@synthesize contentLabel;
+@synthesize leftButton;
+@synthesize rightButton;
+
+- (void)dealloc {
+    [titleLabel release];
+    [contentLabel release];
+    [pointImageView release];
+    [backgroundImageView release];
+    [leftButton release];
+    [rightButton release];
+    [super dealloc];
+}
+
++ (NSString*)getCellIdentifier
+{
+    return @"PlaceOrderCell";
+}
+
++ (CGFloat)getCellHeight
+{
+    return 44.0f;
+}
+
+- (void)setCellWithIndexPath:(NSIndexPath *)aIndexPath
+{
+    self.indexPath = aIndexPath;
+    
+    [leftButton addTarget:self action:@selector(clickLeftButton:) forControlEvents:UIControlEventTouchUpInside];
+    [rightButton addTarget:self action:@selector(clickRightButton:) forControlEvents:UIControlEventTouchUpInside];
+}
+
+- (void)clickLeftButton:(id)sender
+{
+    if ([delegate respondsToSelector:@selector(didClickLeftButton:)]) {
+        [delegate didClickLeftButton:self.indexPath];
+    }
+}
+
+- (void)clickRightButton:(id)sender
+{
+    if ([delegate respondsToSelector:@selector(didClickRightButton:)]) {
+        [delegate didClickRightButton:self.indexPath];
+    }
+}
 
 
 @end
